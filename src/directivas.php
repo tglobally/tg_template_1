37be54f93d8ef507c7124c5409c7c061261e068e
Kevin <kevin.acuna@ivitec.com.mx>
2023-06-27 12:39:04 -0500

    v1.0.0 Se inicializa paquete

---
+++ src/directivas.php
@@ -0,0 +1,3 @@
+<?php
+namespace tglobally\template_1;
+class directivas extends \gamboamartin\template_1\directivas {}
---
+++ src/html.php
@@ -0,0 +1,4 @@
+<?php
+namespace tglobally\template_1;
+
+class html extends \gamboamartin\template_1\html {}
